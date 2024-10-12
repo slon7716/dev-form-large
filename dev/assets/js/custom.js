3961c5fe7a038c66c560968dff1c837b8dab5e3e
Aleksandr <160377@i.ua>
2024-10-12 23:03:27 +0300

    adress

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
    $('select').niceSelect();
    $('[type=tel]').mask('+00 (000) 000-00-00');
+   $('[data-index]').mask('00000');
+
    $('[data-calendar]').wrap('<div class="calendar-container"></div>');
    $('[data-calendar]').datepicker({
       showOn: 'button',
