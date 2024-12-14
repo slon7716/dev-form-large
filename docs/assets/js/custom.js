57f1a9c7426b681a0a5892be4f53ae32cd3d8cd2
Aleksandr <160377@i.ua>
2024-12-14 14:31:51 +0200

    Copied build files to docs

---
+++ docs/assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   $('select').niceSelect();
+   $('[type=tel]').mask('+00 (000) 000-00-00');
+   $('[data-index]').mask('00000');
+
+   $('[data-calendar]').wrap('<div class="calendar-container"></div>');
+   $('[data-calendar]').datepicker({
+      showOn: 'button',
+      buttonImage: 'assets/img/calendar-icon.svg',
+      buttonImageOnly: true,
+    });
+})
